8b3331b18d3e71892aa7467b525cd48c6d280e8f
黃歆芝 <134810151+AAb366661976@users.noreply.github.com>
2026-01-05 18:23:56 +0800

    Add files via upload

---
+++ ImageSendMessage/app.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+#載入LineBot所需要的套件
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+import re
+app = Flask(__name__)
+
+# 必須放上自己的Channel Access Token
+line_bot_api = LineBotApi('yld+flUNs+PHskWmWc3gjrwV+eIfYBWEKeUtzxDa6KUPvqufUoQwnKo81QqH4ei/9VXLZWrZnOVl+ncNVLz1DVXbU570KfkhUJSoUIoyWwy+hkMIByxphsjKChMI8avlB066b/UKFuTEFoblq2OwnAdB04t89/1O/w1cDnyilFU=')
+# 必須放上自己的Channel Secret
+handler = WebhookHandler('343ebd4c37600fac0aa882d0f8eb6625')
+
+line_bot_api.push_message('U743c0f81856abe48d6381f17eae255e2', TextSendMessage(text='你可以開始了'))
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+#訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        image_message = ImageSendMessage(
+            original_content_url='https://www.campus-studio.com/download/flag.jpg',
+            preview_image_url='https://www.campus-studio.com/download/101.jpg'
+        )
+        line_bot_api.reply_message(event.reply_token, image_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+#主程式
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
